b8560b694b1ec84e1d8cfdb6a30adfd9f1c54bed
qb1ty <arsenbro0707@mail.ru>
2026-07-21 18:06:18 +0500

    delete: README.MD удалена

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 export default function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <p className="my-auto">Привет 3D React мир</p>
-    </div>
+    <></>
   )
 }
