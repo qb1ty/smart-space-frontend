f8b76bf3dcbf81cff57359b42e54284407a6cfbf
qb1ty <arsenbro0707@mail.ru>
2026-07-21 17:58:20 +0500

    delete: удалена директория assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 export default function App() {
   return (
-    <></>
+    <div className="flex justify-center items-center h-screen">
+      <p className="my-auto">Привет 3D React мир</p>
+    </div>
   )
 }
