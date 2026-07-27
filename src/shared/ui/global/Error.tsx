f1d3713a42cb391ccf730b0407492fa616f628c1
qb1ty <arsenbro0707@mail.ru>
2026-07-27 20:06:29 +0500

    feat(ui): добавлены shared ui компоненты, глобальные, а так же для навигации

---
+++ src/shared/ui/global/Error.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+import { useRouteError } from "react-router"
+
+export default function Error() {
+    const error = useRouteError()
+
+    useEffect(() => {
+        console.log("Критическая ошибка приложения: ", error)
+    }, [error])
+
+    return (
+        <div className="flex min-h-screen w-full flex-col items-center justify-center bg-(--bg-dark) px-23.25">
+            
+        </div>
+    )
+}
