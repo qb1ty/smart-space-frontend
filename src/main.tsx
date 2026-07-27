89ad7034ceeb10bbd261e70401265c65f1a0a0f8
qb1ty <arsenbro0707@mail.ru>
2026-07-27 20:08:06 +0500

    refactor & chore: удален файл App.tsx, настроен файл main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router'
+import { HelmetProvider } from 'react-helmet-async'
+import { Provider } from "react-redux"
+import { router } from '@/app/router/router.tsx'
+import store from '@/app/store/store.ts' 
 import '@app/styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </Provider>
   </StrictMode>,
 )
