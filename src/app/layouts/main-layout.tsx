f1b364da0604a99f4beb01e8465df0f1f47aec57
qb1ty <arsenbro0707@mail.ru>
2026-07-28 02:09:34 +0500

    feat(layout): закончил верстку main-layout

--- src/app/layouts/main-layout.tsx
+++ src/app/layouts/main-layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import { Navigation } from "@/shared/ui/navigation";
 
 export default function MainLayout() {
     return (
         <div className="flex-1 bg-(--bg-dark)">
+            <Navigation />
             <Outlet />
         </div>
     )
