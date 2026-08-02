e3919a37bd6f6b0383c2bbfb7f7e2be23224617f
qb1ty <arsenbro0707@mail.ru>
2026-08-02 17:10:33 +0500

    chore(index): добавлены новые widgets

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,2 +1,4 @@
+export { default as AboutWidgets } from "./about.widgets"
+export { default as ContactWidgets } from "./contact.widgets"
 export { default as HomeWidgets } from "./home.widgets"
 export { default as NotFoundWidgets } from "./not-found.widgets"
