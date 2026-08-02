9da9b9feffad55821faea5a58241ce20ac18e238
qb1ty <arsenbro0707@mail.ru>
2026-08-02 17:08:40 +0500

    chore(index): добавлены новые страницы

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
+export { default as AboutPages } from "./about.pages"
+export { default as ContactPages } from "./contact.pages"
 export { default as HomePages } from "./home.pages"
 export { default as NotFoundPages } from "./not-found.pages"
