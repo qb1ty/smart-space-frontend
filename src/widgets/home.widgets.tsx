e7c3da22af887f7831864c8abe315cc6f3fe8746
qb1ty <arsenbro0707@mail.ru>
2026-08-05 02:01:39 +0500

    feat(widgets): добавлены 2 новые секции

--- src/widgets/home.widgets.tsx
+++ src/widgets/home.widgets.tsx
@@ -1,7 +1,10 @@
+import { Community, HeroSection } from "@/features/home";
+
 export default function HomeWidgets() {
     return (
         <main className="flex-1 min-h-screen w-full bg-(--bg-color)">
-            
+            <HeroSection />
+            <Community />
         </main>
     )
 }
